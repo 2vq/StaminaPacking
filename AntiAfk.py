878c2f6efccf710b79dc9f23df4adcef57fffa82
drangheta <74454528+drangheta@users.noreply.github.com>
2021-07-22 20:54:04 -0400

    Add files via upload

---
+++ AntiAfk.py
@@ -0,0 +1,44 @@
+import discord
+import json
+import colorama
+import random
+import asyncio
+import datetime
+import time
+import os
+
+from colorama import Fore
+
+client = discord.Client()
+
+with open('./config.json')as f:
+  config = json.load(f)
+
+token = config.get('token')
+
+os.system("title Anti Afk ")
+@client.event
+async def on_connect():
+    print(f'''{Fore.CYAN}
+                                                       AntiAfk
+                                                    discord.gg/ud
+                                                Made by godfather#0001
+                                             https://github.com/drangheta
+                                             Logged in as: {client.user}
+''')
+
+@client.event
+async def on_message(message):
+    if client.user.mentioned_in(message):
+        print(f'''{Fore.CYAN}
+ {message.author} Has pinged you in:
+
+ Group chat or dms: {message.channel}
+
+ Server: {message.guild}
+''')
+        time.sleep(random.choice([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+        ## dont touch anything above u fucking idiot its the WAIT TIME BEFORE REPLYING ##
+        await message.channel.send(random.choice(['focus', 'hi', 'i dont fold son', 'nice 1 lol', 'im asleep zzz'])) # u can add more by doing " , 'msg here' "
+
+client.run(token, bot=False)
